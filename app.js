ed6078dc4dd4d220b20f7c4ed82266236040e0f1
Arny <arne.n.bjerkaas@gmail.com>
2018-07-03 19:15:24 +0200

    round numbers to second decimal point

--- app.js
+++ app.js
@@ -6,15 +6,21 @@
   cel[0].addEventListener('input', function(){
       //display celc celcius value in fah and kel
       fah[0].value = cel[0].value * 9/5 + 32;
+      Math.round(fah[0].value * 100) / 100;
       kel[0].value = parseFloat(cel[0].value) + 273.15;
+      Math.round(kel[0].value * 100) / 100;
   });
   fah[0].addEventListener('input', function(){
       //display fah celcius value in cel and kel
       cel[0].value = fah[0].value * 5/9 - 32;
+      Math.round(cel[0].value * 100) / 100;
       kel[0].value = (parseFloat(fah[0].value) + 459.67) * 5/9;
+      Math.round(kel[0].value * 100) / 100;
   });
   kel[0].addEventListener('input', function(){
       //display kel celcius value in fah and cel
       fah[0].value = kel[0].value * 9/5 - 459.67;
+      Math.round(fah[0].value * 100) / 100;
       cel[0].value = parseFloat(kel[0].value) - 273.15;
+      Math.round(cel[0].value * 100) / 100;
   });
